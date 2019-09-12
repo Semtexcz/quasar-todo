5b9e01de69bdb52bbce3e79332c46c2c5fbfcb1d
semtex <Daniel.Kopecky@techlib.cz>
2019-09-12 12:57:27 +0200

    49. Change Tasks Array into an Object Structure

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -4,8 +4,8 @@
       bordered=''
       separator='')
         q-item(
-          v-for="task in tasks"
-          :key="task.id"
+          v-for="(task, key) in tasks"
+          :key="key"
           @click = "task.ccompleted = !task.ccompleted"
           :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
           clickable
--- src/store/store-tasks.js
+++ src/store/store-tasks.js
@@ -1,27 +1,24 @@
 const state = {
-  tasks: [
-    {
-      id: 1,
+  tasks: {
+    'ID1': {
       name: 'Go to shop',
       completed: false,
       dueDate: '2019/10/12',
       dueTime: '18:30'
     },
-    {
-      id: 2,
+    'ID2': {
       name: 'Get bananas',
       completed: false,
       dueDate: '2019/07/01',
       dueTime: '18:30'
     },
-    {
-      id: 3,
+    'ID3': {
       name: 'Get apples',
       completed: false,
       dueDate: '2019/12/24',
       dueTime: '20:00'
     }
-  ]
+  }
 }
 
 const mutations = {
