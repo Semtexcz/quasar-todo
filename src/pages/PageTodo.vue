221d920178e0bd1d4349a9adcf8a2e8d07f99a29
semtex <Daniel.Kopecky@techlib.cz>
2019-09-12 10:50:07 +0200

    48. Create a Getter for the Tasks Data

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -32,33 +32,11 @@
 </style>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-  data () {
-    return {
-      tasks: [
-        {
-          id: 1,
-          name: 'Go to shop',
-          completed: false,
-          dueDate: '2019/10/12',
-          dueTime: '18:30'
-        },
-        {
-          id: 2,
-          name: 'Get bananas',
-          completed: false,
-          dueDate: '2019/07/01',
-          dueTime: '18:30'
-        },
-        {
-          id: 3,
-          name: 'Get apples',
-          completed: false,
-          dueDate: '2019/12/24',
-          dueTime: '20:00'
-        }
-      ]
-    }
+  computed: {
+    ...mapGetters('tasks', ['tasks'])
   }
 }
 </script>
