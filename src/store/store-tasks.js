c263b9b7c22aadbb18c7c226846de372a72d4ce3
semtex <Daniel.Kopecky@techlib.cz>
2019-09-12 10:36:10 +0200

    47. Add Tasks Data to the Vuex Store

--- src/store/store-tasks.js
+++ src/store/store-tasks.js
@@ -1,5 +1,27 @@
 const state = {
-
+  tasks: [
+    {
+      id: 1,
+      name: 'Go to shop',
+      completed: false,
+      dueDate: '2019/10/12',
+      dueTime: '18:30'
+    },
+    {
+      id: 2,
+      name: 'Get bananas',
+      completed: false,
+      dueDate: '2019/07/01',
+      dueTime: '18:30'
+    },
+    {
+      id: 3,
+      name: 'Get apples',
+      completed: false,
+      dueDate: '2019/12/24',
+      dueTime: '20:00'
+    }
+  ]
 }
 
 const mutations = {
